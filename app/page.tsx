7d406f841194420cdaec511ac2f4c59fefd9dc09
muthassimbilla <122305833+muthassimbilla@users.noreply.github.com>
2025-09-26 12:13:19 +0600

    a

--- app/page.tsx
+++
@@ -1,7 +0,0 @@
-import { redirect } from "next/navigation"
-
-export default function HomePage() {
-  // Middleware will handle the redirect logic
-  // This page should not be reached due to middleware redirects
-  redirect("/login")
-}
